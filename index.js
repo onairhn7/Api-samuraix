bd8ce22ecd6603deac170d3941f3e6ee8388c19b
Nelson Cabrera <80597467+onairhn7@users.noreply.github.com>
2024-03-22 22:08:48 -0600

    actualizada

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {samurai} from './routes/apiSamurai.js'
 import { bando } from './routes/apiBando.js';
 import { estilo } from './routes/apiEstilo.js';
 import { sexo } from './routes/apiSexo.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -10,6 +11,15 @@ const app = express();
 // middlewares
 
 app.use(express.json());
+const corOptions = {
+    origin : 'http://localhost:5173', 
+    credentials : true,
+    methods : ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders : ['Content-Type', 'Authorization']
+}
+
+app.use(cors(corOptions));
+
 const port = 4000;
 
 app.use('/api/samurai', samurai)
